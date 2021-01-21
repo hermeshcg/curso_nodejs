b0fc86c578ac839ad640fcdaa6308c95905cba3f
Hermes Caretta <38593591+HermesCaretta@users.noreply.github.com>
2021-01-21 15:15:34 -0300

    aulas 4 e 5

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -4,73 +4,69 @@
  2 Obter o endereco do usuario pelo Id 
 */
 // importamos um módulo interno do node.js
-const util = require('util')
-const obterEnderecoAsync = util.promisify(obterEndereco)
+const util = require('util');
+const obterEnderecoAsync = util.promisify(obterEndereco);
 
 function obterUsuario() {
-    // quando der algum problea -> reject(ERRO)
-    // quando sucess -> RESOLV
-    return new Promise(function resolvePromise(resolve, reject) {
-        setTimeout(function () {
-            // return reject(new Error('DEU RUIM DE VERDADE!'))
+  // quando der algum problea -> reject(ERRO)
+  // quando sucess -> RESOLV
+  return new Promise(function resolvePromise(resolve, reject) {
+    setTimeout(function () {
+      // return reject(new Error('DEU RUIM DE VERDADE!'))
 
-            return resolve({
-                id: 1,
-                nome: 'Aladin',
-                dataNascimento: new Date()
-            })
-        }, 1000)
-
-    })
+      return resolve({
+        id: 1,
+        nome: 'Aladin',
+        dataNascimento: new Date(),
+      });
+    }, 1000);
+  });
 }
 
 function obterTelefone(idUsuario) {
-    return new Promise(function resolverPromise(resolve, reject) {
-        setTimeout(() => {
-            return resolve({
-                telefone: '1199002',
-                ddd: 11
-            })
-        }, 2000);
-
-    })
+  return new Promise(function resolverPromise(resolve, reject) {
+    setTimeout(() => {
+      return resolve({
+        telefone: '11997002',
+        ddd: 11,
+      });
+    }, 2000);
+  });
 }
 
 function obterEndereco(idUsuario, callback) {
-    setTimeout(() => {
-        return callback(null, {
-            rua: 'dos bobos',
-            numero: 0
-        })
-    }, 2000);
+  setTimeout(() => {
+    return callback(null, {
+      rua: 'dos bobos',
+      numero: 0,
+    });
+  }, 2000);
 }
 
 // 1o passso adicionar a palavra async -> automaticamente ela retornará uma Promise
-main()
+main();
 async function main() {
-    try {
-        console.time('medida-promise')
-        const usuario = await obterUsuario()
-        // const telefone = await obterTelefone(usuario.id)
-        // const endereco = await obterEnderecoAsync(usuario.id)
-        const resultado = await Promise.all([
-            obterTelefone(usuario.id),
-            obterEnderecoAsync(usuario.id)
-        ])
-        const endereco = resultado[1]
-        const telefone = resultado[0]
-        console.log(`
+  try {
+    console.time('medida-promise');
+    const usuario = await obterUsuario();
+    // const telefone = await obterTelefone(usuario.id)
+    // const endereco = await obterEnderecoAsync(usuario.id)
+    const resultado = await Promise.all([
+      obterTelefone(usuario.id),
+      obterEnderecoAsync(usuario.id),
+    ]);
+    const endereco = resultado[1];
+    const telefone = resultado[0];
+    console.log(`
             Nome: ${usuario.nome},
             Telefone: (${telefone.ddd}) ${telefone.telefone},
             Endereco: ${endereco.rua}, ${endereco.numero}
-        `)
-        console.timeEnd('medida-promise')
-
-    } catch (error) {
-        console.error('DEU RUIM', error)
-    }
+        `);
+    console.timeEnd('medida-promise');
+  } catch (error) {
+    console.error('DEU RUIM', error);
+  }
 }
-
 // const usuarioPromise = obterUsuario()
 // // para manipular o sucesso usamos a função .then
 // // para manipular erros , usamos o .catch
@@ -108,7 +104,6 @@ async function main() {
 //     .catch(function (error) {
 //         console.error('DEU RUIM', error)
 //     })
-
 // obterUsuario(function resolverUsuario(error, usuario) {
 //     // null || "" || 0 === false
 //     if (error) {
@@ -125,7 +120,6 @@ async function main() {
 //                 console.error('DEU RUIM em TELEFONE', error)
 //                 return;
 //             }
-
 //             console.log(`
 //              Nome: ${usuario.nome},
 //              Endereco: ${endereco.rua},${endereco.numero}
@@ -135,5 +129,5 @@ async function main() {
 //     })
 // })
 // const telefone = obterTelefone(usuario.id)
-
 // console.log('telefone', telefone)
+//
--- 03-events/index.js
+++ 03-events/index.js
@@ -1,13 +1,10 @@
-const EventEmitter = require('events')
-class MeuEmissor extends EventEmitter {
-
-}
-const meuEmissor = new MeuEmissor()
-const nomeEvento = 'usuario:click'
+const EventEmitter = require('events');
+class MeuEmissor extends EventEmitter {}
+const meuEmissor = new MeuEmissor();
+const nomeEvento = 'usuario:click';
 meuEmissor.on(nomeEvento, function (click) {
-    console.log('um usuario clicou', click)
-})
-
+  console.log('um usuario clicou', click);
+});
 
 // meuEmissor.emit(nomeEvento, 'na barra de rolagem')
 // meuEmissor.emit(nomeEvento, 'no ok')
@@ -18,16 +15,16 @@ meuEmissor.on(nomeEvento, function (click) {
 
 // }, 1000)
 
-const stdin = process.openStdin()
+const stdin = process.openStdin();
 
 function main() {
-    return new Promise(function (resolve, reject) {
-        stdin.addListener('data', function (value) {
-            // console.log(`Voce digitou: ${value.toString().trim()}`)
-            return resolve(value)
-        })
-    })
+  return new Promise(function (resolve, reject) {
+    stdin.addListener('data', function (value) {
+      //console.log(`Voce digitou: ${value.toString().trim()}`);
+      return resolve(value);
+    });
+  });
 }
 main().then(function (resultado) {
-    console.log('resultado', resultado.toString())
-})
+  console.log('resultado', resultado.toString());
+});
